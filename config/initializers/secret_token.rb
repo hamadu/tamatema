c1f875fc2121f717977af7057b9737e6949e6faa
hamadu <dercll1020@gmail.com>
2012-07-07 08:20:47 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlossaryMaker::Application.config.secret_token = 'a67b78aeeac017c0629b6b54d39917bb393f9e0e7b1bcda5cff7e4a914f18802f74703806390c17e176031a7bd59ba2b8fec3ea549556c925983ab94240fb318'
