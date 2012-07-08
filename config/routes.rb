ae2906a41f67755ca3bf648cbadb76b2a61d7946
hamadu <dercll1020@gmail.com>
2012-07-09 03:13:05 +0900

    static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 GlossaryMaker::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  get "static_pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
