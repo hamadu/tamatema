492d9fca680c173985cb1042c208b48a8d5be21f
hamadu <dercll1020@gmail.com>
2012-07-09 14:13:33 +0900

    password test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password_digest, :temporary_key
+  attr_accessible :email, :name, :password, :password_confirmation, :temporary_key
+  has_secure_password
   
   validates :name, presence:true, length:{ maximum:32 }
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence:true, format:{ with:VALID_EMAIL_REGEX }
+  validates :email, presence:true, format:{ with:VALID_EMAIL_REGEX }, uniqueness:true
+  
+  validates :password, presence:true, length:{ minimum:6 }
+  validates :password_confirmation, presence:true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,27 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
 describe User do
   before {
     @user = User.new(
       name: "Example User",
-      email: "user@example.com"
+      email: "user@example.com",
+      password: "a" * 6,
+      password_confirmation: "a" * 6
     )
   }
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:temporary_key) }
+
+  it { should respond_to(:authenticate) }
+
   it { should be_valid }
   
   describe "name" do
@@ -46,6 +55,46 @@ describe User do
         before { @user.email = address }
         it { @user.should_not be_valid }
       end
+    end
+    describe "duplicate email" do
+      before {
+        @user2 = @user.dup
+        @user2.save
+      }
+      it { should_not be_valid }
+    end
+  end
+  
+  describe "password" do
+    describe "password is not present" do
+      before { @user.password = @user.password_confirmation = "" }
+      it { should_not be_valid }
+    end
+    describe "password is too short" do
+      before { @user.password = @user.password_confirmation = "a" * 5 }
+      it { should_not be_valid }
+    end
+    describe "password don't match confirmation" do
+      before {
+        @user.password = "a" * 6
+        @user.password_confirmation = "b" * 6
+      }
+      it { should_not be_valid }
+    end
+  end
+  
+  describe "authentication" do
+    before { @user.save }
+    let(:found_user) { User.find_by_email(@user.email) }
+
+    describe "valid" do
+      it { should == found_user.authenticate(@user.password) }
+    end
+
+    describe "invalid" do
+      let(:user_with_invalid_password) { found_user.authenticate("different_password") }
+      it { should_not == user_with_invalid_password }
+      specify { user_with_invalid_password.should be_false }
     end
   end
 end
