df5bdc7d6175756d8864483b30f92bdc4f466750
hamadu <dercll1020@gmail.com>
2012-07-11 01:58:16 +0900

    menu after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by_email(params[:session][:email])
-    if user and user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       sign_in user
       redirect_to user
     else
